c4d34ce828de80d8785f820895e6660b3b1a9e93
Shaik Mahaboob jani <92629496+skmahaboobjani@users.noreply.github.com>
2021-11-03 12:25:22 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// for while
+// for each and for(x in obj)
+// these loops are used to iterate  array and an object
+
+// install 
+let arr=[ 1,2,3,4,5,5];
+arr.forEach((element,index) {
+  console.log(element)
+  console.log(index)
+})
